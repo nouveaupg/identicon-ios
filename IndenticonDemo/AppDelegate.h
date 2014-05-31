f52a31ae56c21f74ea24d4d73f3d45c04b99856f
John Hill <jack@Johns-MacBook-Pro.local>
2014-05-31 11:32:13 -0400

    Initial Commit

---
+++ IndenticonDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IndenticonDemo
+//
+//  Created by John Hill on 5/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IndenticonDemo/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  IndenticonDemo
+//
+//  Created by John Hill on 5/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
